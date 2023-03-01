89366d6632d19e94c5e70382957de30e90d442c4
손상준 <tkdwns1245@opennaru.com>
2023-03-01 17:27:27 +0900

    1. 로그인 페이지 생성 2.부트스트랩 cdn 추가 3.jsconfig.json 추가(../../components -> components 로 접근가능)

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,13 @@
 import { Fragment } from "react";
 import styles from "../styles/Header.module.css";
 import Image from "next/image";
+import { useRouter } from 'next/router';
+import NextLink from 'next/link';
 export default function Header() {
+  const router = useRouter();
+  const currentPath = router.asPath;
+
+  if (currentPath.includes('account')) return null;
   return (
     <Fragment>
       <header className={styles.header}>
@@ -14,7 +20,9 @@ export default function Header() {
           className={styles.logo}
         />
         <div></div>
-        <div className={styles.login}>로그인/ 회원가입</div>
+        <NextLink href="/account/login">
+          <div className={styles.login}>로그인/ 회원가입</div>
+        </NextLink>
       </header>
     </Fragment>
   );
--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <link href="//netdna.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />

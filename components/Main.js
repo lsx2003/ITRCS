a3c74a6f8c868435205b75c3e67dd6654e2c76af
손상준 <tkdwns1245@gmail.com>
2023-03-26 18:34:28 +0900

    main 페이지퍼블리싱

--- components/Main.js
+++ components/Main.js
@@ -8,35 +8,30 @@ export default function Main() {
   return (
     <Fragment>
       <main className={styles.container}>
-        <div className={styles.content}>
-          키워드를 통해
-          <br />
-          언론기사와 판례를 검색하세요.
-          <SearchBar></SearchBar>
-        </div>
-        <div>
-          <Image
-            className={styles.img}
-            src="/main3.jpg"
-            alt="main"
-            width={500}
-            height={500}
+          <Image 
+          src="/img/background-image1.png"
+          className={styles.backgroundImage}
+          fill
           />
-          <Image
-            className={styles.img}
-            src="/main2.jpg"
-            alt="main"
-            width={500}
-            height={500}
-          ></Image>
-          <Image
-            className={styles.img}
-            src="/main1.jpg"
-            alt="main"
-            width={500}
-            height={500}
-          />
-        </div>
+          <div className={styles.backgroundWrapper}></div>
+          <div className={styles.mainContentConatiner}>
+            <div className={styles.contentTitle}>하나의 촛불은 미약하지만</div>
+            <div className={styles.contentTitle} style={{textAlign:"end",marginTop:"-15px"}}>함께하면 어둠을 밝힙니다.</div>
+            <div className={styles.mainContentWarpper}>
+교사의 아동학대 고소 건수 : 연간 건
+<br/>
+그 중 무혐의 비율 98%
+<br/>
+인정 판정의 사례 : “속담을 외우게 해.. 징역 6개월에 집행 유예
+
+<br/>당신이 생각하는 진정한 교육이란 무엇입니까?
+            </div>
+            <div className={styles.mainContentButtonWrapper}>
+              <div className={styles.mainContentButton}>
+                판례 보러 가기  
+              </div>
+            </div>         
+          </div>
       </main>
     </Fragment>
   );
